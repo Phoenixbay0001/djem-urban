77cd070a8143cc10a605b4474847ad95d9bbb8d0
Ruslan <62209841+Phoenixbay0001@users.noreply.github.com>
2026-03-25 00:37:06 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,4 @@
-"use client";
-
-import "./globals.css";
-import { useEffect } from "react";
-import Lenis from "lenis";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-
-  useEffect(() => {
-    const lenis = new Lenis({ smooth: true });
-
-    function raf(time: number) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <html lang="ru">
       <body>{children}</body>
