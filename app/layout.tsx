3f15d606e532a51afa5b98a0eae03698ff6b54a6
Ruslan <62209841+Phoenixbay0001@users.noreply.github.com>
2026-03-25 00:19:48 +0500

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import "./globals.css";
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  useEffect(() => {
+    const lenis = new Lenis({ smooth: true });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <html lang="ru">
+      <body>{children}</body>
+    </html>
+  );
+}
